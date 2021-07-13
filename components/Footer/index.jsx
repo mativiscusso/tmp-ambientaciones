66469464523db3a632db2745caf9b43e085bba7f
mativiscusso <mativiscusso@gmail.com>
2021-07-13 11:20:14 -0300

    add footer in mobile version

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,3 +1,58 @@
+import Image from "next/image";
+import logo from "svg/logo.svg";
+import instagram from "svg/instagram.svg";
+import facebook from "svg/facebook.svg";
+import Link from "next/link";
+import styles from "./Footer.module.scss";
+
 export default function Footer() {
-    return <footer>Soy el footer</footer>;
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.items}>
+                <p className={styles.marca}>
+                    <Image src={logo} width={40} height={40} alt="logo TMP" />
+                    &nbsp; ambientaciones
+                </p>
+                <p>Calle falsa 123 - Rosario - Argentina</p>
+                <p>email@email.com</p>
+                <span>
+                    <Image
+                        src={instagram}
+                        width={30}
+                        height={30}
+                        alt="logo TMP"
+                    />
+                    <Image
+                        src={facebook}
+                        width={30}
+                        height={30}
+                        alt="logo TMP"
+                    />
+                </span>
+            </div>
+            <div className={styles.items}>
+                <p>
+                    <Link href="/">
+                        <a>¿Qué hacemos?</a>
+                    </Link>
+                </p>
+                <p>
+                    <Link href="/">
+                        <a>Blog</a>
+                    </Link>
+                </p>
+                <p>
+                    <Link href="/">
+                        <a>Nuestros trabajos</a>
+                    </Link>
+                </p>
+                <p>
+                    <Link href="/">
+                        <a>Contacto</a>
+                    </Link>
+                </p>
+            </div>
+            <div className={styles.items}>Google Form LINK</div>
+        </footer>
+    );
 }
