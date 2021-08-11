2d597d4a7c1729c0a1bf03e8c9424ae373f142e7
mativiscusso <mativiscusso@gmail.com>
2021-08-11 14:58:52 -0300

    last changes

--- components/Admin/EventList/index.jsx
+++ components/Admin/EventList/index.jsx
@@ -8,17 +8,23 @@ import Loading from "components/Loading";
 import router from "next/router";
 import Link from "next/link";
 
+const pageSize = 5;
 export default function EventList({ admin }) {
     const [events, setEvents] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [open, setOpen] = useState(false);
     const [evtToDelete, setEvtToDelete] = useState(undefined);
 
+    const [page, setPage] = useState(1);
+    const [rangePage, setRangePage] = useState({
+        init: 0,
+        end: page * pageSize,
+    });
+
     useEffect(() => {
         setIsLoading(true);
         fetchAllEvents()
             .then((result) => {
-                console.log(result);
                 setEvents(result);
                 setIsLoading(false);
             })
@@ -27,7 +33,6 @@ export default function EventList({ admin }) {
 
     const handleDelete = async () => {
         setOpen(false);
-        console.log(evtToDelete);
         await deleteDocumentOfCollection(evtToDelete, "events");
         router.reload();
     };
@@ -66,38 +71,106 @@ export default function EventList({ admin }) {
 
                 <tbody>
                     {events &&
-                        events.map((event, i) => (
-                            <tr key={event.id}>
-                                <th>{i + 1}</th>
-                                <th>
-                                    {event.style
-                                        ? event.style.toUpperCase()
-                                        : "Sin Estilo"}
-                                </th>
-                                <th>{event.title}</th>
-                                <th style={{ textTransform: "uppercase" }}>
-                                    {event.category}
-                                </th>
-                                <th>
-                                    <button id={event.id} onClick={handleEdit}>
-                                        <Tooltip text="Editar">
-                                            <EditIcon />
-                                        </Tooltip>
-                                    </button>
+                        events
+                            .slice(rangePage.init, rangePage.end)
+                            .map((event, i) => (
+                                <tr key={event.id}>
+                                    <th>{rangePage.init + i + 1}</th>
+                                    <th>
+                                        {event.style
+                                            ? event.style.toUpperCase()
+                                            : "Sin Estilo"}
+                                    </th>
+                                    <th>{event.title}</th>
+                                    <th style={{ textTransform: "uppercase" }}>
+                                        {event.category}
+                                    </th>
+                                    <th>
+                                        <button
+                                            id={event.id}
+                                            onClick={handleEdit}
+                                        >
+                                            <Tooltip text="Editar">
+                                                <EditIcon />
+                                            </Tooltip>
+                                        </button>
 
-                                    <button
-                                        id={event.id}
-                                        onClick={handleOpenModal}
-                                    >
-                                        <Tooltip text="Eliminar">
-                                            <DeleteIcon />
-                                        </Tooltip>
-                                    </button>
-                                </th>
-                            </tr>
-                        ))}
+                                        <button
+                                            id={event.id}
+                                            onClick={handleOpenModal}
+                                        >
+                                            <Tooltip text="Eliminar">
+                                                <DeleteIcon />
+                                            </Tooltip>
+                                        </button>
+                                    </th>
+                                </tr>
+                            ))}
                 </tbody>
             </table>
+            <p>
+                Mostrar eventos ( {rangePage.init} - {rangePage.end} )
+            </p>
+            <div>
+                <button
+                    onClick={() => {
+                        setRangePage((prevState) => ({
+                            init: prevState.init - pageSize,
+                            end: prevState.end - pageSize,
+                        }));
+                    }}
+                    disabled={rangePage.init === 0}
+                    className={styles.buttonsPagination}
+                >
+                    <svg
+                        height="21"
+                        viewBox="0 0 21 21"
+                        width="21"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <g
+                            fill="none"
+                            fillRule="evenodd"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            transform="translate(5 6)"
+                        >
+                            <path d="m8.5 8.5-4-4 4-4" />
+                            <path d="m4.5 8.5-4-4 4-4" />
+                        </g>
+                    </svg>
+                </button>
+                <button
+                    onClick={() => {
+                        setRangePage((prevState) => ({
+                            init: prevState.init + pageSize,
+                            end: prevState.end + pageSize,
+                        }));
+                    }}
+                    disabled={rangePage.end >= events.length}
+                    className={styles.buttonsPagination}
+                >
+                    <svg
+                        height="21"
+                        viewBox="0 0 21 21"
+                        width="21"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <g
+                            fill="none"
+                            fillRule="evenodd"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            transform="translate(7 6)"
+                        >
+                            <path d="m.5 8.5 4-4-4-4" />
+                            <path d="m4.5 8.5 4-4-4-4" />
+                        </g>
+                    </svg>
+                </button>
+            </div>
             {open && (
                 <div
                     className={styles.modal}
